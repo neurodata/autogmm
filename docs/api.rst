73ed5db940bbd55d80cac5d9ba14b4f3c684fa44
TingshanLiu <tliu68@jhmi.edu>
2025-09-13 12:55:38 -0400

    fix Docs

--- docs/api.rst
+++ docs/api.rst
@@ -4,3 +4,4 @@ API Reference
 .. automodule:: autogmm.autogmm
     :members:
     :undoc-members:
+    :show-inheritance:
--- docs/conf.py
+++ docs/conf.py
@@ -8,3 +8,17 @@ author = "Tingshan Liu"
 extensions = ["sphinx.ext.autodoc", "numpydoc"]
 html_theme = "sphinx_rtd_theme"
 autodoc_default_options = {"members": True, "inherited-members": True}
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "numpydoc",
+]
+
+autosummary_generate = True
+
+numpydoc_show_class_members = False
+numpydoc_class_members_toctree = False
+
+suppress_warnings = ["ref.ref"]
